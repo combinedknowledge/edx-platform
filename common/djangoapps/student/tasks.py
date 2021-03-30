edx-olive/edx-platform-old

[JuniperOeX-319] Change getting link for templates related to current site instead of default, fix logo path for templates

common/djangoapps/student/tasks.py
@@ -20,7 +20,7 @@ log = logging.getLogger('edx.celery.task')
 
 
 @task(bind=True)
-def send_activation_email(self, msg_string, from_address=None):
+def send_activation_email(self, msg_string, site_id=None, from_address=None):
     """
     Sending an activation email to the user.
     """
@@ -37,7 +37,7 @@ def send_activation_email(self, msg_string, from_address=None):
 
     dest_addr = msg.recipient.email_address
 
-    site = Site.objects.get_current()
+    site = Site.objects.get(id=site_id) if site_id else Site.objects.get_current()
     user = User.objects.get(username=msg.recipient.username)
 
     try:

combinedknowledge/edx-platform

common/djangoapps/student/tasks.py
@@ -20,7 +20,7 @@ log = logging.getLogger('edx.celery.task')
 
 
 @task(bind=True, name='student.send_activation_email')
-def send_activation_email(self, msg_string, from_address=None):
+def send_activation_email(self, msg_string, from_address=None, site_id=None):
     """
     Sending an activation email to the user.
     """
@@ -37,7 +37,7 @@ def send_activation_email(self, msg_string, from_address=None):
 
     dest_addr = msg.recipient.email_address
 
-    site = Site.objects.get_current()
+    site = Site.objects.get(id=site_id) if site_id else Site.objects.get_current()
     user = User.objects.get(username=msg.recipient.username)
 
     try: